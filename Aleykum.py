404c79849d755ed6a3ee51a3f7a6f448f4a93dca
Tavhidjon <Vohidzodatavhid@.com>
2024-09-13 18:16:46 +0500

    Classes

---
+++ Aleykum.py
@@ -0,0 +1,29 @@
+# class Calculator:
+#     def __init__(self,a,b):
+#         self.a=a
+#         self.b=b
+#     def add(self):
+#         print(f"{self.a+self.b}")
+#     def sub(self):
+#         print(f"{self.a-self.b}")
+#     def mul(self):
+#         print(f"{self.a*self.b}")
+#     def div(self):
+#         print(f"{self.a/self.b}")
+#     def pow(self):
+#         print(f"{self.a**self.b}")
+#     def mod(self):
+#         print(f"{self.a%self.b}")
+# obj1=Calculator(int(input()),int(input()))
+# obj1.sub()
+# obj1.add()
+# obj1.mul()
+# obj1.div()
+# obj1.mod()
+# obj1.pow()
+
+
+
+
+
+
---
+++ Salom.py
@@ -0,0 +1,54 @@
+# class Person:
+#     def __init__(self,name):
+#         self.name=name
+#     def greet(self):
+#         print(f"Hello my name is {self.name}")
+# obj1=Person(input())
+# obj1.greet()
+
+
+
+
+
+
+
+# class Rectangle:
+#     def __init__(self,length,width):
+#         self.length=length
+#         self.width=width
+#     def area(self):
+#         print(f"self.length+self.width}")
+# obj1=Rectangle(int(input()),int(input()))
+# obj1.area()
+
+
+
+
+
+# class Circle:
+#     pi=3.14
+#     def __init__(self,radius):
+#         self.radius=radius
+        
+#     def area(self):
+#         print(Circle.pi*(self.radius**2))
+# obj1=Circle(int(input()))
+# obj1.area()
+
+
+
+
+
+
+# class Car:
+#     total_cars=0
+#     def __init__(self):
+#         Car.total_cars+=1
+#     @classmethod
+#     def returner(cls):
+#         print(cls.total_cars)
+# obj1 = Car()
+# obj1 = Car()
+# obj1 = Car()
+# obj1 = Car()
+# obj1.returner()
